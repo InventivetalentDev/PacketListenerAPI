a3c652402eb6003225466c701e99aca68050134b
inventivetalent <inventivetalent@inventivegames.de>
2016-03-05 03:45:32 +0100

    add javadoc to ChannelWrapper

--- PacketListenerAPI/API/src/org/inventivetalent/packetlistener/channel/ChannelWrapper.java
+++ PacketListenerAPI/API/src/org/inventivetalent/packetlistener/channel/ChannelWrapper.java
@@ -41,14 +41,23 @@ public class ChannelWrapper<T> {
 		this.channel = channel;
 	}
 
+	/**
+	 * @return the raw channel object
+	 */
 	public T channel() {
 		return this.channel;
 	}
 
+	/**
+	 * @return the remote {@link SocketAddress}
+	 */
 	public SocketAddress getRemoteAddress() {
 		return null;
 	}
 
+	/**
+	 * @return the local {@link SocketAddress}
+	 */
 	public SocketAddress getLocalAddress() {
 		return null;
 	}
