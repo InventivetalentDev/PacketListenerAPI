29d10d6f1e9532d90bf9cdacffc6e33a9438d13b
inventivetalent <inventivetalent@inventivegames.de>
2017-01-31 09:37:28 +0100

    add updater

--- API/src/org/inventivetalent/packetlistener/PacketListenerPlugin.java
+++ API/src/org/inventivetalent/packetlistener/PacketListenerPlugin.java
@@ -3,6 +3,8 @@ package org.inventivetalent.packetlistener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.inventivetalent.apihelper.APIManager;
+import org.inventivetalent.update.spiget.SpigetUpdate;
+import org.inventivetalent.update.spiget.UpdateCallback;
 
 public class PacketListenerPlugin extends JavaPlugin {
 
@@ -21,6 +23,19 @@ public class PacketListenerPlugin extends JavaPlugin {
 			Bukkit.getPluginManager().disablePlugin(this);
 			return;
 		}
+
+		SpigetUpdate updater = new SpigetUpdate(this, 2930);
+		updater.checkForUpdate(new UpdateCallback() {
+			@Override
+			public void updateAvailable(String s, String s1, boolean b) {
+				getLogger().info("There is a new version available: https://r.spiget.org/2930");
+			}
+
+			@Override
+			public void upToDate() {
+				getLogger().info("Plugin is up-to-date");
+			}
+		});
 
 		//Initialize this API if the plugin got enabled
 		APIManager.initAPI(PacketListenerAPI.class);
