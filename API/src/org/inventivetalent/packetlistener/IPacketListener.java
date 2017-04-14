0c4c7496a82e0b07d5a329eff8d0b486be2a595c
InventiveBot <git-bot@inventivetalent.org>
2017-04-14 09:51:36 +0000

    Javadoc for #20

--- API/src/org/inventivetalent/packetlistener/IPacketListener.java
+++
@@ -1,8 +0,0 @@
-package org.inventivetalent.packetlistener;
-
-public interface IPacketListener {
-
-	Object onPacketSend(Object receiver, Object packet, org.bukkit.event.Cancellable cancellable);
-
-	Object onPacketReceive(Object sender, Object packet, org.bukkit.event.Cancellable cancellable);
-}
--- README.md
+++
@@ -1,7 +0,0 @@
-# PacketListenerAPI
-
-[![Build Status](https://travis-ci.org/InventivetalentDev/PacketListenerAPI.svg?branch=master)](https://travis-ci.org/InventivetalentDev/PacketListenerAPI)
-
-API to modify Minecraft packets
-
-[SpigotMC page](https://r.spiget.org/2930/)
